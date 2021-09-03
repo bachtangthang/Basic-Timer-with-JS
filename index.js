a1de613292be5ffc72c640e6845ead2987e9238c
bachtangthang <thang5217113@gmail.com>
2021-09-03 23:51:12 +0700

    add class Timer, button for further exphaination

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('testing the fifth times!!!');
+class Timer{
+    constructor(durationInput, startButton, pauseButton){
+        this.durationInput = durationInput;
+        this.startButton =startButton;
+        this.pauseButton = pauseButton;
+        this.startButton.addEventListener('click', this.start)
+    }
+
+    start(){
+
+    }
+}
+
+const durationInput = document.querySelectorAll('#duration');
+const startButton = document.querySelectorAll('#start');
+const pauseButton= document.querySelectorAll('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
